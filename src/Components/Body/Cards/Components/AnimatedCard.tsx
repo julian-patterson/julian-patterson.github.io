a7d35c22681cc3d62389936a8d857185495f3e4b
julian-patterson <julianedwardpatterson@gmail.com>
2023-02-09 21:24:24 -0500

    trying something else

--- src/Components/Body/Cards/Components/AnimatedCard.tsx
+++ src/Components/Body/Cards/Components/AnimatedCard.tsx
@@ -8,6 +8,7 @@ interface IAnimatedCard {
   body: any;
   animationOffset: any;
   footer?: any;
+  visibility: boolean;
 }
 
 const HiddenCard = styled(Card)`
@@ -23,30 +24,30 @@ const ShownCard = styled(Card)`
 `;
 
 const AnimatedCard: FC<IAnimatedCard> = (props) => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      } else {
-        entry.target.classList.remove("show");
-      }
-    });
-  });
+  if (props.visibility == true) {
+    const TestCard = styled(Card)`
+      opacity: 1;
+      transform: translateY(0);
+    `;
+  }
 
-  const hiddenElements = document.querySelectorAll(".hidden");
-  hiddenElements.forEach((el) => observer.observe(el));
+  if (props.visibility == false) {
+    const TestCard = styled(Card)`
+      opacity: 0;
+      filter: blur(5px);
+      transform: translateY(-100%);
+      transition: all 1s;
+    `;
+  }
 
   return (
-    <div className="section">
-      <Card size="sm">
-        <CardHeader>
-          <Heading size="md">{props.title}</Heading>
-        </CardHeader>
-        <CardBody>{props.body}</CardBody>
-        {props.footer}
-      </Card>
-    </div>
+    <Card>
+      <CardHeader>
+        <Heading size="md">{props.title}</Heading>
+      </CardHeader>
+      <CardBody>{props.body}</CardBody>
+      {props.footer}
+    </Card>
   );
 };
 
