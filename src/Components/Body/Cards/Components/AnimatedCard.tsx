a607992c8ed14fece3a5793487991a2084535525
julian-patterson <julianedwardpatterson@gmail.com>
2023-02-10 13:38:02 -0500

    CSS styling is working TODO: - Intersection Observer if statements

--- src/Components/Body/Cards/Components/AnimatedCard.tsx
+++ src/Components/Body/Cards/Components/AnimatedCard.tsx
@@ -1,53 +1,39 @@
+import "./Animations.css";
+
 import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
 
 import { FC } from "react";
-import styled from "styled-components";
 
 interface IAnimatedCard {
   title: string;
   body: any;
   animationOffset: any;
   footer?: any;
-  visibility: boolean;
 }
 
-const HiddenCard = styled(Card)`
-  opacity: 0;
-  filter: blur(5px);
-  transform: translateY(-100%);
-  transition: all 1s;
-`;
-
-const ShownCard = styled(Card)`
-  opacity: 1;
-  transform: translateY(0);
-`;
-
 const AnimatedCard: FC<IAnimatedCard> = (props) => {
-  if (props.visibility == true) {
-    const TestCard = styled(Card)`
-      opacity: 1;
-      transform: translateY(0);
-    `;
-  }
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.className.replace("hidden", "show");
+      } else {
+        entry.target.className.replace("show", "hidden");
+      }
+    });
+  });
 
-  if (props.visibility == false) {
-    const TestCard = styled(Card)`
-      opacity: 0;
-      filter: blur(5px);
-      transform: translateY(-100%);
-      transition: all 1s;
-    `;
-  }
-
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((el) => observer.observe(el));
   return (
-    <Card>
-      <CardHeader>
-        <Heading size="md">{props.title}</Heading>
-      </CardHeader>
-      <CardBody>{props.body}</CardBody>
-      {props.footer}
-    </Card>
+    <div className="hidden">
+      <Card>
+        <CardHeader>
+          <Heading size="md">{props.title}</Heading>
+        </CardHeader>
+        <CardBody>{props.body}</CardBody>
+        {props.footer}
+      </Card>
+    </div>
   );
 };
 
